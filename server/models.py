3b94111585cf3ecb8370e27c18639347f3c79c97
ajin3830 <ajin3830@gmail.com>
2023-03-12 23:49:30 -0400

    code along

--- server/models.py
+++ server/models.py
@@ -1,3 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Owner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+
+    pets = db.relationship('Pet', backref='owner')
+
+    def __repr__(self):
+        return f'<Pet Owner {self.name}'
+    
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    species = db.Column(db.String)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey("owner.id"))
+
+    def __repr__(self):
+        return f'<Pet {self.name} {self.species}'
